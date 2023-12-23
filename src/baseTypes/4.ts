1237a582af3917c1b50a5bc549ce1efbae6d68af
Kateryna <k.churuta@gmail.com>
2023-12-23 09:49:44 +0000

    baseTypes

--- src/baseTypes/4.ts
+++ src/baseTypes/4.ts
@@ -2,7 +2,10 @@
   Як переписати його в TypeScript, використовуючи концепцію кортежів, 
   щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
 */
+let person : [string, number];
+person = ['Max', 21];
 
-let person = ['Max', 21];
+let name :string = person[0];
+let age:number = person[1]
 
-export {};
+export {name,age,person};
